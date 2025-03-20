322fbf6fc27e37bdc72eabaa351e15e35474d990
LeeNilson <99687743+LeeNilson@users.noreply.github.com>
2025-03-20 09:59:46 -0300

    First Commit
    First Commit

---
+++ exRaio.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main{
+double area, pi, raio, n 
+scanf("%lf%lf%lf%lf", &area, &pi, &raio, &n);
+printf("Digite o raio da esfera");
+scanf("")
+n = 3.14159
+pi = 3.14
+area = pi * n 
+
+
+
+
+
+
+
+    return 0;
+}
